bf57261323139b1ef3c3d8aac293aa5e56171147
forrester-aidan <forrester.aidan@ufl.edu>
2024-12-07 12:40:13 -0500

    first project complete

--- medals.py
+++ medals.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 teams = pd.read_csv("teams.csv")
 teams = teams[["team", "country", "year", "athletes", "age", "prev_medals", "medals"]]
 
@@ -16,4 +17,12 @@ predictors = ["athletes", "prev_medals"]
 reg.fit(train[predictors], train["medals"])
 
 predictions = reg.predict(test[predictors])
-print(predictions.shape)
+test["predictions"] = predictions
+test.loc[test["predictions"] < 0, "predictions"] = 0
+test["predictions"] = test["predictions"].round()
+
+error = mean_absolute_error(test["medals"], test["predictions"])
+test["predictions"] = predictions
+
+sns.lmplot(x="athletes", y="predictions", data=test, fit_reg=True, ci=None)
+plt.show()
